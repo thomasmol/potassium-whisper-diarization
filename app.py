92191873e57945e0d00ef6c81e14f635cfa42b5a
Thomas Mol <email@thomasmol.com>
2023-04-01 21:10:33 +0200

    fixes + async

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from potassium import Potassium, Request, Response
+from potassium import Potassium, Request, Response, send_webhook
 
 import os
 import time
-import json
 import wave
 import torch
 import base64
@@ -18,7 +17,6 @@ from pyannote.audio.pipelines.speaker_verification import PretrainedSpeakerEmbed
 
 app = Potassium("whisper_diarization")
 
-
 # @app.init runs at startup, and loads models into the app's context
 @app.init
 def init():
@@ -28,19 +26,18 @@ def init():
     embedding_model = PretrainedSpeakerEmbedding(
         "speechbrain/spkrec-ecapa-voxceleb", device=device
     )
-    context = {"model": model}
-    context = {"embedding_model": embedding_model}
+    context = {"model": model, "embedding_model": embedding_model}
 
     return context
 
-
-# @app.handler runs for every call
-@app.handler("/")
+# @app.async_handler runs for every call
+@app.async_handler("/")
 def handler(context: dict, request: Request) -> Response:
     prompt = request.json.get("prompt")
     filename = request.json.get("filename")
     file = request.json.get("file")
     num_speakers = request.json.get("num_speakers")
+    webhook_url = request.json.get("webhook_url")
     model = context.get("model")
     embedding_model = context.get("embedding_model")
 
@@ -59,96 +56,95 @@ def handler(context: dict, request: Request) -> Response:
     with open(filename, "wb") as f:
         f.write(file_data)
 
-    def convert_time(secs):
-        return datetime.timedelta(seconds=round(secs))
-
-    def speech_to_text(filepath, whisper_model, num_speakers, prompt):
-        model = whisper.load_model(whisper_model)
-        time_start = time.time()
-
-        try:
-            _, file_ending = os.path.splitext(f"{filepath}")
-            print(f"file enging is {file_ending}")
-            audio_file_wav = filepath.replace(file_ending, ".wav")
-            print("-----starting conversion to wav-----")
-            os.system(
-                f'ffmpeg -i "{filepath}" -ar 16000 -ac 1 -c:a pcm_s16le "{audio_file_wav}"'
-            )
-        except Exception as e:
-            raise RuntimeError("Error converting audio")
-
-        # Get duration
-        with contextlib.closing(wave.open(audio_file_wav, "r")) as f:
-            frames = f.getnframes()
-            rate = f.getframerate()
-            duration = frames / float(rate)
-        print(f"conversion to wav ready, duration of audio file: {duration}")
-
-        # Transcribe audio
-        print("starting whisper")
-        options = dict(beam_size=5, best_of=5)
-        transcribe_options = dict(task="transcribe", **options)
-        result = model.transcribe(
-            audio_file_wav, **transcribe_options, initial_prompt=prompt
-        )
-        segments = result["segments"]
-        print("done with whisper")
-
-        try:
-            # Create embedding
-            def segment_embedding(segment):
-                audio = Audio()
-                start = segment["start"]
-                # Whisper overshoots the end timestamp in the last segment
-                end = min(duration, segment["end"])
-                clip = Segment(start, end)
-                waveform, sample_rate = audio.crop(audio_file_wav, clip)
-                return embedding_model(waveform[None])
-
-            print("starting embedding")
-            embeddings = np.zeros(shape=(len(segments), 192))
-            for i, segment in enumerate(segments):
-                embeddings[i] = segment_embedding(segment)
-            embeddings = np.nan_to_num(embeddings)
-            print(f"Embedding shape: {embeddings.shape}")
-
-            # Assign speaker label
-            clustering = AgglomerativeClustering(num_speakers).fit(embeddings)
-            labels = clustering.labels_
-            for i in range(len(segments)):
-                segments[i]["speaker"] = "SPEAKER " + str(labels[i] + 1)
-
-            # Make output
-            output = []  # Initialize an empty list for the output
-            for segment in segments:
-                # Append the segment to the output list
-                output.append(
-                    {
-                        "start": str(convert_time(segment["start"])),
-                        "end": str(convert_time(segment["end"])),
-                        "speaker": segment["speaker"],
-                        "text": segment["text"],
-                    }
-                )
-
-            print("done with embedding")
-            time_end = time.time()
-            time_diff = time_end - time_start
-
-            system_info = f"""-----Processing time: {time_diff:.5} seconds-----"""
-            print(system_info)
-            os.remove(audio_file_wav)
-            return output
-
-        except Exception as e:
-            os.remove(audio_file_wav)
-            raise RuntimeError("Error Running inference with local model", e)
-
-    transcription = speech_to_text(filename, model, num_speakers, prompt)
+    transcription = speech_to_text(filename,model, embedding_model, num_speakers, prompt)
     os.remove(filename)
     print(f'{filename} removed, done with inference')
-    return Response(json={"segments": transcription}, status=200)
+    send_webhook(url=webhook_url, json={"segments": transcription})
+    return
 
+def convert_time(secs):
+    return datetime.timedelta(seconds=round(secs))
+
+def speech_to_text(filepath, model, embedding_model, num_speakers, prompt):
+    time_start = time.time()
+
+    try:
+        _, file_ending = os.path.splitext(f"{filepath}")
+        print(f"file enging is {file_ending}")
+        audio_file_wav = filepath.replace(file_ending, ".wav")
+        print("-----starting conversion to wav-----")
+        os.system(
+            f'ffmpeg -i "{filepath}" -ar 16000 -ac 1 -c:a pcm_s16le "{audio_file_wav}"'
+        )
+    except Exception as e:
+        raise RuntimeError("Error converting audio")
+
+    # Get duration
+    with contextlib.closing(wave.open(audio_file_wav, "r")) as f:
+        frames = f.getnframes()
+        rate = f.getframerate()
+        duration = frames / float(rate)
+    print(f"conversion to wav ready, duration of audio file: {duration}")
+
+    # Transcribe audio
+    print("starting whisper")
+    options = dict(beam_size=5, best_of=5)
+    transcribe_options = dict(task="transcribe", **options)
+    result = model.transcribe(
+        audio_file_wav, **transcribe_options, initial_prompt=prompt
+    )
+    segments = result["segments"]
+    print("done with whisper")
+
+    try:
+        # Create embedding
+        def segment_embedding(segment):
+            audio = Audio()
+            start = segment["start"]
+            # Whisper overshoots the end timestamp in the last segment
+            end = min(duration, segment["end"])
+            clip = Segment(start, end)
+            waveform, sample_rate = audio.crop(audio_file_wav, clip)
+            return embedding_model(waveform[None])
+
+        print("starting embedding")
+        embeddings = np.zeros(shape=(len(segments), 192))
+        for i, segment in enumerate(segments):
+            embeddings[i] = segment_embedding(segment)
+        embeddings = np.nan_to_num(embeddings)
+        print(f"Embedding shape: {embeddings.shape}")
+
+        # Assign speaker label
+        clustering = AgglomerativeClustering(num_speakers).fit(embeddings)
+        labels = clustering.labels_
+        for i in range(len(segments)):
+            segments[i]["speaker"] = "SPEAKER " + str(labels[i] + 1)
+
+        # Make output
+        output = []  # Initialize an empty list for the output
+        for segment in segments:
+            # Append the segment to the output list
+            output.append(
+                {
+                    "start": str(convert_time(segment["start"])),
+                    "end": str(convert_time(segment["end"])),
+                    "speaker": segment["speaker"],
+                    "text": segment["text"],
+                }
+            )
+
+        print("done with embedding")
+        time_end = time.time()
+        time_diff = time_end - time_start
+
+        system_info = f"""-----Processing time: {time_diff:.5} seconds-----"""
+        print(system_info)
+        os.remove(audio_file_wav)
+        return output
+
+    except Exception as e:
+        os.remove(audio_file_wav)
+        raise RuntimeError("Error Running inference with local model", e)
 
 if __name__ == "__main__":
     app.serve()
